5fa5297401e42ddbb10893ae60fab0fddfea5600
andygock <andy@agock.com>
2014-03-06 03:57:50 +1100

    More basic progress, bug fixes

--- jsi.py
+++ jsi.py
@@ -21,7 +21,7 @@ or a .torrent file as a POST request where the file parameter is named "torrent_
 """
 
 import sys
-import urllib, urllib2, xmltodict, json, argparse
+import urllib, urllib2, xmltodict, json, argparse, poster
 
 class bcolors:
     HEADER = '\033[95m'
@@ -45,12 +45,14 @@ class JustSeedIt():
     url = "https://api.justseed.it"
     
     def __init__(self):
+        # Obtain API key
         try:
             f = open('.justseedit_api_key','r') 
             key = f.read()
             self.api_key = key.strip()
         except IOError:
             # Could not read api key from file
+            # USe default api_key, which is actually an empty string
             pass
         
         self.ratio = 1.0
@@ -60,9 +62,6 @@ class JustSeedIt():
         print "Error:", message
         print "Quitting."
         sys.exit()
-
-    def set_ratio(self, ratio):
-        self.ratio = ratio
         
     def xml_from_file(self, file):
         f = open(file,'r') 
@@ -75,7 +74,7 @@ class JustSeedIt():
         return xml
   
     def api(self, page, data={}):
-        """ Make a API call
+        """ Make a API call using multipart/form-data POST
             Returns XML response on success or False on error
         """
         data['api_key'] = self.api_key
@@ -87,8 +86,19 @@ class JustSeedIt():
                 print "{:>15}: {:}".format(key, value)
         
         try:
-            post_data = urllib.urlencode( data )
-            req = urllib2.Request(self.url + page, post_data)
+            # for application/x-www-form-urlencoded
+            #post_data = urllib.urlencode( data ) 
+            #req = urllib2.Request(self.url + page, post_data)
+            
+            # for multipart/form-data
+            poster.streaminghttp.register_openers()
+            post_data, headers = poster.encode.multipart_encode(data)
+
+            #if self.debug:
+            #    print headers
+            
+            req = urllib2.Request(self.url + page, post_data, headers)
+            
             response = urllib2.urlopen(req)
             xml_response = response.read()
             self.xml_response = xml_response
@@ -99,6 +109,8 @@ class JustSeedIt():
         
         if self.check_server_response(xml_response):
             self.error = False
+            if self.debug:
+                print xml_response
             return xml_response
         else:
             self.error = True
@@ -129,8 +141,9 @@ class JustSeedIt():
         print "Adding from file '{}' with ratio {}".format(filename, self.ratio)
         
         try:
-            f = open(filename,'r')
+            f = open(filename,'rb')
             data = f.read()
+            
         except IOError:
             print "Could not open file '{0}'".format(filename)
             return
@@ -175,20 +188,28 @@ if __name__ == "__main__":
     
     # Set up CLI arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--magnet", help="magnet link", type=str, nargs=1)
-    parser.add_argument("--torrent-file", type=str, help='add single torrent file')
-    parser.add_argument("--list", action='store_true', help='list torrents')
+    parser.add_argument("--magnet", "-m", help="magnet link", type=str, nargs=1)
+    parser.add_argument("--torrent-file", "-t", type=str, help='add single torrent file')
+    parser.add_argument("--list", "-l", action='store_true', help='list torrents')
     parser.add_argument("--list-xml", action='store_true', help='list torrents in raw XML format')
     parser.add_argument("--download-links", type=str, help='get download links')
-    parser.add_argument("--pause", action='store_true', help='pause when finished')
-    parser.add_argument("--ratio", type=float, help='set ratio')
+    parser.add_argument("--pause", "-p", action='store_true', help='pause when finished')
+    parser.add_argument("--ratio", "-r", type=float, help='set ratio')
+    parser.add_argument("--verbose", "-v", action='store_true', help='verbose mode')
+    parser.add_argument("--debug", "-d", action='store_true', help='debug mode')
         
     args = parser.parse_args()
     
     jsi = JustSeedIt();
     
+    if args.debug:
+        jsi.debug = True
+ 
+    if args.verbose:
+        jsi.verbose = True
+        
     if args.ratio:
-        jsi.set_ratio(args.ratio)
+        jsi.ratio = args.ratio
     
     if args.magnet:
         jsi.add_magnet(args.magnet[0])
